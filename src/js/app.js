cbd3fdb355a0af9bc2e5cf9935da76c21f1c9de8
rserrano169 <rserrano169@gmail.com>
2022-10-05 22:51:14 -0400

    Add Section and TextBlock components. Update styles.

--- src/js/app.js
+++ src/js/app.js
@@ -5,29 +5,80 @@ import ReactDOM from 'react-dom/client';
 import resumePdf from '../assets/Resume_Raul_Serrano.pdf';
 import resumePng from '../assets/Resume_Raul_Serrano.png';
 
+class Section extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: {
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+      },
+      text: props.text,
+    };
+  }
+
+  render() {
+    return(
+      <div style={this.state.style}>
+        <TextBlock text={this.state.text}/>
+        <Resume />
+      </div>
+    );
+  }
+}
+
 class Resume extends Component {
   constructor(props) {
     super(props);
     this.state = {
       style: {
-        maxWidth: '50%',
+        span: {
+          padding: 20,
+          width: '50%',
+        },
+        img: {
+          maxWidth: '100%',
+        },
       },
     };
   }
 
   render() {
     return (
-      <a
-        href={resumePdf}
-        target="_blank"
-      >
-        <img
-          alt="Resume PDF"
-          src={resumePng}
-          style={this.state.style}
+      <span style={this.state.style.span}>
+        <a
+          href={resumePdf}
+          target="_blank"
         >
-        </img>
-      </a>
+          <img
+            alt="Resume PDF"
+            src={resumePng}
+            style={this.state.style.img}
+          >
+          </img>
+        </a>
+      </span>
+    );
+  }
+}
+
+class TextBlock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: {
+        fontSize: 30,
+        padding: 20,
+        width: '50%',
+      },
+      text: this.props.text,
+    };
+  }
+
+  render() {
+    return(
+      <div style={this.state.style}>{this.state.text}</div>
     );
   }
 }
@@ -37,9 +88,8 @@ class Header extends Component {
     super(props);
     this.state = {
       style: {
-        color: '#fff',
         fontSize: 40,
-        paddingBottom: 20,
+        padding: 20,
         textAlign: 'center',
       },
     };
@@ -56,6 +106,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: {
+        textBlocks: [
+          'I am a full-stack web developer with a background in JavaScript, Ruby, and Python, and frameworks such as Node, React, and Rails. I am looking to join a creative and ambitious team, where I can contribute to challenging projects and continue to grow as a developer.',
+          'In addition to my programming skills, I gained invaluable experience with teamwork and discipline when I attended the United States Military Academy at West Point. Willingness to dive headfirst into challenging situations while maintaining quick and critical thinking, was a constant requirement at “The Point”, and has shaped who I am. I apply this enduring life skill to new challenges in programming.',
+          'My most recent roles are Front-end Software Engineer at Teachley, LLC and Quality Assurance Manager at Kiswe Mobile Inc. I enjoy working with others, problem solving, the challenge of meeting high standards for product quality, and improving the efficiency of production processes.'
+        ],
+      },
       style: {
         height: '100%',
         padding: 20,
@@ -71,7 +128,7 @@ class App extends Component {
         style={this.state.style}
       >
         <Header />
-        <Resume />
+        <Section text={this.state.data.textBlocks[2]}/>
       </div>
     );
   }
